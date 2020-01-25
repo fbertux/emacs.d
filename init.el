fa424c59f18296cbf617ef7b05da2e43520d0e28
Fabio Berton <fbberton@gmail.com>
2020-01-25 14:41:14 -0300

    Add files
    Signed-off-by: Fabio Berton <fbberton@gmail.com>

---
+++ init.el
@@ -0,0 +1,30 @@
+;;; init.el --- -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+;;; Early settings
+(menu-bar-mode -1)
+(setq gc-cons-threshold 67108864)
+(setq large-file-warning-threshold 100000000)
+(setq package-enable-at-startup nil)
+(setq site-run-file nil)
+(unless (display-graphic-p)
+  (push '(menu-bar-lines . 0) default-frame-alist))
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+(org-babel-load-file "~/.emacs.d/settings.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yaml-mode ws-butler use-package undo-tree systemd smex smartparens sane-term rainbow-delimiters projectile pkgbuild-mode pdf-tools org-tree-slide org-bullets ob-async mmm-mode markdown-mode magit latex-preview-pane json-mode grip-mode go-guru go-eldoc gnu-elpa-keyring-update git-timemachine git-gutter flycheck flx-ido expand-region dts-mode dockerfile-mode diminish company color-theme-sanityinc-tomorrow browse-kill-ring auctex aggressive-indent))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
