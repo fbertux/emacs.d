6331fc7613ea17b9b4fc161dafc3f1464a2619c8
Fabio Berton <fbberton@gmail.com>
2020-04-21 11:44:39 -0300

    init.el: Update package-selected-packages
    Signed-off-by: Fabio Berton <fbberton@gmail.com>

--- init.el
+++ init.el
@@ -14,3 +14,17 @@
 (push '(vertical-scroll-bars) default-frame-alist)
 
 (org-babel-load-file "~/.emacs.d/settings.org")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (go-guru go-eldoc go-mode latex-preview-pane auctex pdf-tools mmm-mode sane-term ox-hugo ob-async htmlize smex flx-ido aggressive-indent yaml-mode systemd pkgbuild-mode json-mode dts-mode dockerfile-mode forge git-gutter git-timemachine magit flycheck company undo-tree ws-butler smartparens rainbow-delimiters expand-region browse-kill-ring projectile gnu-elpa-keyring-update diminish zenburn-theme use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
