3f44a7ad2b5abcc69ca0efba42a403c8b292b235
Fabio Berton <fbberton@gmail.com>
2020-06-21 15:20:48 -0300

    init.el: Sync package-selected-packages list
    Signed-off-by: Fabio Berton <fbberton@gmail.com>

--- init.el
+++ init.el
@@ -20,7 +20,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (org-journal qt-pro-mode go-guru go-eldoc go-mode latex-preview-pane auctex pdf-tools mmm-mode sane-term ox-hugo ob-async htmlize smex flx-ido aggressive-indent yaml-mode systemd pkgbuild-mode json-mode dts-mode dockerfile-mode forge git-gutter git-timemachine magit flycheck company undo-tree ws-butler smartparens rainbow-delimiters expand-region browse-kill-ring projectile gnu-elpa-keyring-update diminish zenburn-theme use-package))))
+    (zenburn-theme yaml-mode ws-butler use-package undo-tree systemd smex smartparens smart-mode-line sane-term rainbow-delimiters qt-pro-mode projectile pkgbuild-mode pdf-tools ox-hugo org-noter org-journal ob-async mmm-mode latex-preview-pane json-mode htmlize go-guru go-eldoc gnu-elpa-keyring-update git-timemachine git-gutter forge flycheck flx-ido expand-region dts-mode dockerfile-mode diminish company browse-kill-ring auctex aggressive-indent))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
