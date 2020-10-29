e2e818e2366b12f0566aee721b70d17e9090bc40
Fabio Berton <fbberton@gmail.com>
2020-10-28 22:35:57 -0300

    Add Olivetti mode
    Signed-off-by: Fabio Berton <fbberton@gmail.com>

--- init.el
+++ init.el
@@ -19,7 +19,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zenburn-theme yaml-mode ws-butler use-package undo-tree systemd smex smartparens sane-term rainbow-delimiters qt-pro-mode qml-mode projectile pkgbuild-mode pdf-tools ox-hugo ox-gfm org-noter org-journal ob-async nginx-mode mmm-mode magit-todos latex-preview-pane json-mode ibuffer-tramp htmlize go-guru go-eldoc gnu-elpa-keyring-update git-timemachine git-gutter forge flycheck flx-ido expand-region dts-mode dockerfile-mode docker-tramp diminish company browse-kill-ring auctex)))
+   '(zenburn-theme yaml-mode ws-butler use-package undo-tree systemd smex smartparens sane-term rainbow-delimiters qt-pro-mode qml-mode projectile pkgbuild-mode pdf-tools ox-hugo ox-gfm org-super-agenda org-noter org-journal olivetti ob-async mmm-mode magit-todos latex-preview-pane json-mode ibuffer-tramp htmlize gnu-elpa-keyring-update git-timemachine git-gutter forge flycheck flx-ido expand-region elfeed dts-mode dockerfile-mode docker-tramp diminish company browse-kill-ring bongo auctex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
