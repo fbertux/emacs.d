885cd45265259c3714854691e41185ef641db75f
Fabio Berton <fbberton@gmail.com>
2020-12-11 11:31:11 -0300

    git-timemachine: Remove package
    I don't use this package.
    
    Signed-off-by: Fabio Berton <fbberton@gmail.com>

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(zenburn-theme yaml-mode ws-butler use-package undo-tree systemd smex smartparens sane-term rainbow-delimiters qt-pro-mode qml-mode projectile pkgbuild-mode pdf-tools ox-hugo ox-gfm org-super-agenda org-noter org-journal olivetti ob-async nix-mode mmm-mode magit-todos latex-preview-pane json-mode ibuffer-tramp htmlize gnu-elpa-keyring-update git-timemachine git-gutter forge flycheck flx-ido expand-region elfeed dts-mode dockerfile-mode docker-tramp diminish company browse-kill-ring bongo auctex)))
+   '(zenburn-theme yaml-mode ws-butler use-package undo-tree systemd smex smartparens sane-term rainbow-delimiters qt-pro-mode qml-mode projectile pkgbuild-mode pdf-tools ox-hugo ox-gfm org-super-agenda org-noter org-journal olivetti ob-async nix-mode mmm-mode magit-todos latex-preview-pane json-mode ibuffer-tramp htmlize gnu-elpa-keyring-update git-gutter forge flycheck flx-ido expand-region elfeed dts-mode dockerfile-mode docker-tramp diminish company browse-kill-ring bongo auctex)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
