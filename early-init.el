a06172e905042c9a6b5771a1769a871ec672e298
Fabio Berton <fbberton@gmail.com>
2021-02-16 15:53:08 -0300

    early-init.el: display-startup-time
    Signed-off-by: Fabio Berton <fbberton@gmail.com>

--- early-init.el
+++ early-init.el
@@ -15,3 +15,12 @@
 (setq inhibit-splash-screen t)
 (setq package-enable-at-startup nil)
 (setq site-run-file nil)
+
+(defun efs/display-startup-time ()
+  (message "Emacs loaded in %s with %d garbage collections."
+           (format "%.2f seconds"
+                   (float-time
+                   (time-subtract after-init-time before-init-time)))
+           gcs-done))
+
+(add-hook 'emacs-startup-hook #'efs/display-startup-time)
